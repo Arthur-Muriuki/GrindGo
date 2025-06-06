99c42d10099498daf4c1509abd0a270ace2e10fd
Arthur <rockymuriuki@gmail.com>
2025-06-06 18:39:43 +0300

    first commit

--- Routineapp/models.py
+++ Routineapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Routine(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    day = models.CharField(max_length=10)  # e.g. 'Monday'
+    time = models.TimeField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title} on {self.day} at {self.time}"
