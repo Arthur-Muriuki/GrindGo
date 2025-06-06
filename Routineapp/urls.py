55b42ad8dcfab54bbb298414e6ce1e7fb3e956b3
Arthur <rockymuriuki@gmail.com>
2025-06-07 00:20:21 +0300

    first commit

--- Routineapp/urls.py
+++ Routineapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('create/', views.routine_create, name='routine_create'),
     path('update/<int:pk>/', views.routine_update, name='routine_update'),
     path('delete/<int:pk>/', views.routine_delete, name='routine_delete'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
